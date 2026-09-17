76452e8c455a92e81f64820657323fbe119e4f48
Daniil Solonko <wwwsokol999@gmail.com>
2026-09-18 01:03:49 +0300

    Add provider and backend configuration to main

---
+++ providers.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.226.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+  }
+
+  required_version = "~>1.12.0"
+
+  backend "s3" {
+    bucket = "terraform-state-wwwsokol999-05"
+    key    = "terraform.tfstate"
+    region = "ru-central1"
+
+    use_lockfile = true
+
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.default_zone
+}
